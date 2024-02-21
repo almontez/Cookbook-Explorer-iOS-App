99d69c2eab8c17ea349966e857c07f63c6ae935c
Angela Montez <angelali.montez@gmail.com>
2024-02-21 13:08:35 -0800

    Programmatically Launch VC

---
+++ cookbook-explorer/cookbook-explorer/RecipesListController.swift
@@ -0,0 +1,20 @@
+//
+//  RecipesListController.swift
+//  cookbook-explorer
+//
+//  Created by Angela Li Montez on 2/21/24.
+//
+
+import UIKit
+
+class RecipesListController: UIViewController {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
